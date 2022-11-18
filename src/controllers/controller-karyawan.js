baa90e04ffbb86af1547aa3adfea2a5fba881274
istiyawan <istiyawan.pratiwanto@nindyakarya.co.id>
2022-11-18 22:03:24 +0700

    first commit

---
+++ src/controllers/controller-karyawan.js
@@ -0,0 +1,124 @@
+const config = require('../configs/database');
+const mysql = require('mysql');
+const pool = mysql.createPool(config);
+
+pool.on('error',(err)=> {
+    console.error(err);
+});
+
+module.exports ={
+
+    getIndex(req, res){
+                res.send({ 
+                    success: true, 
+                    message: 'hello boys',
+                });
+    },
+    // Ambil data semua karyawan
+    getDataKaryawan(req,res){
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                SELECT * FROM customers;
+                `
+            , function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil ambil data!',
+                    data: results 
+                });
+            });
+            connection.release();
+        })
+    },
+    // Ambil data karyawan berdasarkan ID
+    getDataKaryawanByID(req,res){
+        let id = req.params.id;
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                SELECT * FROM customers WHERE id = ?;
+                `
+            , [id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil ambil data!',
+                    data: results
+                });
+            });
+            connection.release();
+        })
+    },
+    // Simpan data karyawan
+    addDataKaryawan(req,res){
+        let data = {
+            name : req.body.name,
+            address: req.body.address,
+        }
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                INSERT INTO customers SET ?;
+                `
+            , [data],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil tambah data!',
+                });
+            });
+            connection.release();
+        })
+    },
+    // Update data karyawan
+    editDataKaryawan(req,res){
+        let dataEdit = {
+            name : req.body.name,
+            address: req.body.address,
+        }
+        let id = req.body.id
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                UPDATE customers SET ? WHERE id = ?;
+                `
+            , [dataEdit, id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil edit data!',
+                });
+            });
+            connection.release();
+        })
+    },
+    // Delete data karyawan
+    deleteDataKaryawan(req,res){
+        let id = req.body.id
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                DELETE FROM customers WHERE id = ?;
+                `
+            , [id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil hapus data!'
+                });
+            });
+            connection.release();
+        })
+    }
+}
